6139d897dc94d2e543271de7308ee3ad9f57450a
Amam Raushan <amanraushan0@gmail.com>
2025-11-29 11:57:13 +0530

    url

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(cors({
     // origin: 'http://localhost:3000',
 
     // production
-    origin: 'https://fluffy-centaur-c88e70.netlify.app/',
+    origin: 'https://fluffy-centaur-c88e70.netlify.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
